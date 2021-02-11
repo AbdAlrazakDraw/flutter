a574bb91f07358ea9350e52c8e5920159a5c85b2
AbdAlrazakDraw <41840691+AbdAlrazakDraw@users.noreply.github.com>
2021-02-11 20:07:53 +0200

    Update remote_services.dart

--- lib/services/remote_services.dart
+++ lib/services/remote_services.dart
@@ -3,12 +3,10 @@ import 'package:task1/Model/imgaes.dart';
 
 class RemoteServices {
   static var client = http.Client();
-  static Future<List<images>> fetchProducts(int items) async {
-    String url='https://picsum.photos/v2/list?limit='+items.toString();
+  static Future<List<images>> fetchProducts(int page,int items) async {
+    String url='https://picsum.photos/v2/list?page=$page+&limit='+items.toString();
     print(url);
-    var response = await client.get(
-        url);
-
+    var response = await client.get(url);
     if (response.statusCode == 200) {
       print(response.statusCode);
       var jsonString = response.body;
@@ -19,5 +17,3 @@ class RemoteServices {
       return null;
     }
   }
-}
-
