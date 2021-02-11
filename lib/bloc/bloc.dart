4fa8a03254f61e966f867f6b5b8854e7084c3f0f
AbdAlrazakDraw <41840691+AbdAlrazakDraw@users.noreply.github.com>
2021-02-11 20:06:24 +0200

    Update bloc.dart

--- lib/bloc/bloc.dart
+++ lib/bloc/bloc.dart
@@ -6,15 +6,26 @@ import 'package:task1/services/remote_services.dart';
 
 class BlocIm extends Bloc<imEvent, imState>{
   RemoteServices repository;
-  BlocIm(imState initialState,this.repository) : super(initialState);
+  bool First;
+  List<images> result;
+  BlocIm(imState initialState,this.repository,this.First) : super(initialState);
   @override
   int get initialState => 0;
 
   @override
   Stream<imState> mapEventToState(imEvent event) async* {
     if (event is Fetchimages) {
-      var result= await RemoteServices.fetchProducts(event.pagesNumber); {
-        yield Loaded(posts: result);
+      if(First){
+        yield LoadingState();
+        result=[];
+        First=false;
+        print(result.length);
+      }
+      result= await RemoteServices.fetchProducts(event.pageNumber,event.itemsNumber); {
+        if (result.isNotEmpty) {
+          print(result.length);
+          yield Loaded(posts: result);
+        }
       }}
     if(event is getAllimageEvent)
     {
@@ -22,14 +33,11 @@ class BlocIm extends Bloc<imEvent, imState>{
       List<images> imgs=[];
       for(int i=0;i<30;i++)
       {
-        images im=new images(url: "https://picsum.photos/id/$i/400/400");
+        images im=new images(url: "https://picsum.photos/id/$i/420/420");
         imgs.add(im);
         print('true');
       }
       yield getDataState(img: imgs);
     }
-
   }
-
-
 }
